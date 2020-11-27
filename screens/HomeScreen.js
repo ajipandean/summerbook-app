1cfdb382f4576096706b14663af3aac301c46598
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-11-27 21:51:34 +0800

    added header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Header, Icon, Divider} from 'react-native-elements';
+import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
 
 export default function HomeScreen() {
+  const {openDrawer} = useNavigation();
+  const styles = StyleSheet.create({
+    brand: {fontSize: 20},
+  });
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Header
+        placement="left"
+        backgroundColor="#fff"
+        centerComponent={() => <Text style={styles.brand}>Summerbook</Text>}
+        leftComponent={() => (
+          <TouchableOpacity onPress={() => openDrawer()}>
+            <Icon
+              name="menu"
+              type="material-community"
+            />
+          </TouchableOpacity>
+        )}
+        rightComponent={() => (
+          <Icon
+            name="dots-horizontal"
+            type="material-community"
+          />
+        )}
+      />
+      <Divider/>
     </View>
   );
 }
