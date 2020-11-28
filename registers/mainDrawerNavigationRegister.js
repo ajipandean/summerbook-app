844f8568a0e380241bf7a2309ac3439883ae5a60
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-11-28 14:51:53 +0800

    removed about

--- registers/mainDrawerNavigationRegister.js
+++ registers/mainDrawerNavigationRegister.js
@@ -3,7 +3,6 @@ import {View} from 'react-native';
 
 import HomeScreen from '../screens/HomeScreen';
 
-const About = () => <View/>;
 const Settings = () => <View/>;
 
 export default [
@@ -11,12 +10,6 @@ export default [
     name: 'Home',
     icon: 'home-variant-outline',
     component: HomeScreen,
-    options: {},
-  },
-  {
-    name: 'About',
-    icon: 'information-outline',
-    component: About,
     options: {},
   },
   {
