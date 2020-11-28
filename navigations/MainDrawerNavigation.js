c2daad29a4e4ae3b4e30717d65fd986c0ecc58e0
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-11-28 14:31:24 +0800

    moved screen header to drawer navigator

--- navigations/MainDrawerNavigation.js
+++ navigations/MainDrawerNavigation.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {Header, Divider} from 'react-native-elements';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Text, StyleSheet} from 'react-native';
 
+import MoreMenu from '../components/android/MoreMenu';
+import DrawerToggle from '../components/android/DrawerToggle';
+import DrawerContent from '../components/android/DrawerContent';
 import mainDrawerNavigationRegister from '../registers/mainDrawerNavigationRegister';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigation() {
+  const styles = StyleSheet.create({
+    brand: {fontSize: 20},
+  });
   return (
     <Drawer.Navigator
       intialRouteName="Home"
+      drawerContent={DrawerContent}
+      overlayColor={0}
+      drawerType="slide"
+      screenOptions={{
+        headerShown: true,
+        header: ({scene}) => (
+          <Fragment>
+            <Header
+              placement="left"
+              backgroundColor="#fff"
+              rightComponent={() => <MoreMenu/>}
+              centerComponent={() => <Text style={styles.brand}>{scene.route.name}</Text>}
+              leftComponent={() => <DrawerToggle navigation={scene.descriptor.navigation}/>}
+            />
+            <Divider/>
+          </Fragment>
+        ),
+      }}
+      drawerStyle={{
+        borderRightWidth: 1,
+        borderColor: '#e0e0e0',
+      }}
       drawerContentOptions={{
         labelStyle: {fontWeight: 'bold'},
-        itemStyle: {
-          marginVertical: 2,
-          borderRadius: 8,
-        },
+        itemStyle: {marginVertical: 2},
       }}
     >
       {mainDrawerNavigationRegister.map((s) => (
@@ -24,9 +51,10 @@ export default function DrawerNavigation() {
           name={s.name}
           component={s.component}
           options={{
-            drawerIcon: ({focused, size, color}) => (
+            ...s.options,
+            drawerIcon: ({size, color}) => (
               <MaterialCommunityIcons
-                name={focused ? s.icon.active : s.icon.inactive}
+                name={s.icon}
                 size={size}
                 color={color}
               />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {Header, Icon, Divider} from 'react-native-elements';
-import {View, ScrollView, TouchableOpacity, StyleSheet, Text} from 'react-native';
+import {View, ScrollView} from 'react-native';
 
-import MoreMenu from '../components/android/MoreMenu';
 import BooksCarousel from '../components/android/home/BooksCarousel';
 
 export default function HomeScreen() {
-  const {openDrawer, navigate} = useNavigation();
-  const styles = StyleSheet.create({
-    brand: {fontSize: 20},
-  });
+  const {navigate} = useNavigation();
   return (
     <View style={{flex: 1}}>
-      <Header
-        placement="left"
-        backgroundColor="#fff"
-        centerComponent={<Text style={styles.brand}>Summerbook</Text>}
-        leftComponent={(
-          <TouchableOpacity onPress={() => openDrawer()}>
-            <Icon
-              name="menu"
-              type="material-community"
-            />
-          </TouchableOpacity>
-        )}
-        rightComponent={<MoreMenu/>}
-      />
-      <Divider/>
       <ScrollView>
         <BooksCarousel navigate={navigate}/>
       </ScrollView>
