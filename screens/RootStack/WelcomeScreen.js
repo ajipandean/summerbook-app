670ce44c69be1d6b2bbb1b7f87a645e2f281d40c
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-11-30 15:06:32 +0800

    developed welcome screen

--- screens/RootStack/WelcomeScreen.js
+++ screens/RootStack/WelcomeScreen.js
@@ -1,11 +1,62 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Text} from 'react-native-elements';
+import {useNavigation} from '@react-navigation/native';
+import {View, StyleSheet, Image} from 'react-native';
+import {Text, SocialIcon, ThemeConsumer} from 'react-native-elements';
+
+import bookLoverIllustration from '../../assets/illustrations/book_lover.png';
 
 export default function WelcomeScreen() {
+  const {navigate} = useNavigation();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+    },
+    illustration: {
+      width: '80%',
+      height: 240,
+      resizeMode: 'cover',
+      marginBottom: 16,
+    },
+    title: {marginBottom: 16},
+  });
   return (
-    <View>
-      <Text>Welcome screen</Text>
-    </View>
+    <ThemeConsumer>
+      {({theme}) => (
+        <View style={styles.container}>
+          <Image
+            style={styles.illustration}
+            source={bookLoverIllustration}
+          />
+          <View>
+            <Text h3 h3Style={styles.title}>Welcome to Summerbook</Text>
+            <SocialIcon
+              button
+              type="envelope"
+              underlayColor={theme.colors.primary}
+              style={{backgroundColor: theme.colors.primary}}
+              title="Login with Email"
+              onPress={() => navigate('Login')}
+            />
+            <SocialIcon
+              button
+              type="google"
+              title="Login with Google"
+              onPress={() => {}}
+            />
+            <Text
+              style={{
+                marginTop: 20,
+                textAlign: 'center',
+              }}
+            >
+              Don&apos;t have an account? Sign up
+            </Text>
+          </View>
+        </View>
+      )}
+    </ThemeConsumer>
   );
 }
